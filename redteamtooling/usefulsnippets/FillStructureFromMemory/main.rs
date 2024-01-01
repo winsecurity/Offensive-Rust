8de881b63d0925f1b252959edb1175ad33c5ecc3
nikhil-polymath <nagasainikhil100@gmail.com>
2024-01-01 17:30:34 +0530

    Update main.rs

--- redteamtooling/usefulsnippets/FillStructureFromMemory/main.rs
+++ redteamtooling/usefulsnippets/FillStructureFromMemory/main.rs
@@ -1,22 +1,45 @@
-pub fn ReadStringFromMemory(prochandle: *mut c_void, base: *const c_void) -> String {
+pub fn FillStructureFromMemory<T>(dest: &mut T,src: *const c_void,prochandle: *mut c_void,) -> usize {
     unsafe {
-        let mut i: isize = 0;
-        let mut s = String::new();
-        loop {
-            let mut a: [u8; 1] = [0];
-            ReadProcessMemory(
-                prochandle,
-                (base as isize + i) as *const c_void,
-                a.as_mut_ptr() as *mut c_void,
-                1,
-                std::ptr::null_mut(),
-            );
+        let bytestoread: usize = std::mem::size_of::<T>();
+        ////println!("size of structure is {}",bytestoread);
+        let mut buffer: Vec<u8> = vec![0; bytestoread];
+        let mut byteswritten = 0;
+        let res = ReadProcessMemory(
+            prochandle,
+            src,
+            buffer.as_mut_ptr() as *mut c_void,
+            bytestoread,
+            &mut byteswritten,
+        );
+        ////println!("array being filled: {:x?}",&buffer);
+        FillStructureFromArray(dest, &buffer);
 
-            if a[0] == 0 || i == 50 {
-                return s;
-            }
-            s.push(a[0] as char);
-            i += 1;
-        }
+        return byteswritten;
+    }
+}
+
+
+pub fn FillStructureFromArray<T, U>(base: &mut T, arr: &[U]) -> usize {
+
+    unsafe {
+        //println!("{}",std::mem::size_of::<T>());
+        //println!("{}",std::mem::size_of_val(arr));
+        /*if std::mem::size_of::<T>() != std::mem::size_of_val(arr) {
+            println!("{}", std::mem::size_of::<T>());
+            println!("{}", std::mem::size_of_val(arr));
+            panic!("sizes are not equal to copy");
+        }*/
+
+        let mut handle = GetCurrentProcess();
+        let mut byteswritten = 0;
+        let res = WriteProcessMemory(
+            handle,
+            base as *mut _ as *mut c_void,
+            arr as *const _ as *const c_void,
+            std::mem::size_of::<T>(),
+            &mut byteswritten,
+        );
+
+        return byteswritten;
     }
 }
