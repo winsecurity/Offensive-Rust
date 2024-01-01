01812b7c1945f5057d6a9ec8b557168618f35e02
nikhil-polymath <nagasainikhil100@gmail.com>
2024-01-01 17:30:06 +0530

    Create main.rs

---
+++ redteamtooling/usefulsnippets/FillStructureFromMemory/main.rs
@@ -0,0 +1,22 @@
+pub fn ReadStringFromMemory(prochandle: *mut c_void, base: *const c_void) -> String {
+    unsafe {
+        let mut i: isize = 0;
+        let mut s = String::new();
+        loop {
+            let mut a: [u8; 1] = [0];
+            ReadProcessMemory(
+                prochandle,
+                (base as isize + i) as *const c_void,
+                a.as_mut_ptr() as *mut c_void,
+                1,
+                std::ptr::null_mut(),
+            );
+
+            if a[0] == 0 || i == 50 {
+                return s;
+            }
+            s.push(a[0] as char);
+            i += 1;
+        }
+    }
+}
